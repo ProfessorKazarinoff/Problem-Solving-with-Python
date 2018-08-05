12b15c7a78fe47111cf0a564c93777ebec87aca7
Peter Kazarinoff <peter.kazarinoff@pcc.edu>
2018-08-04 20:38:03 -0700

    all notebooks added

---
+++ notebooks/04-Jupyter-Notebooks/hello.py
@@ -0,0 +1,2 @@
+print('This code was run from a seperate Python file')
+print('Hellow from the file hello.py')
---
+++ notebooks/05-Functions-and-Modules/myfunctions.py
@@ -0,0 +1,10 @@
+# myfunctions.py
+
+def plustwo(n):
+    n = n + 2
+    return n
+
+
+def falldist(t, g=9.81):
+    d = 0.5 * g * t**2
+    return d
